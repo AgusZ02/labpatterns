d21144148e31d2e855e24840d7d7debc7ab8bade
Olarizu <olarizuga@gmail.com>
2023-11-12 20:59:51 +0100

    3 y 4

--- src/adapter2/Covid19PacientTableModelAdapter.java
+++ src/adapter2/Covid19PacientTableModelAdapter.java
@@ -1,13 +1,17 @@
 package adapter2;
 
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
 
 import domain.Covid19Pacient;
+import domain.Symptom;
 
 public class Covid19PacientTableModelAdapter extends AbstractTableModel {
 	  protected Covid19Pacient pacient;
 	  protected String[] columnNames =
 	    new String[] {"Symptom", "Weight" };
+	  protected List<Covid19Pacient> pacients;
 
 	  public Covid19PacientTableModelAdapter(Covid19Pacient p) {
 	    this.pacient=p;
@@ -15,21 +19,40 @@ public class Covid19PacientTableModelAdapter extends AbstractTableModel {
 
 	  public int getColumnCount() {
 	    // Challenge!
-		 return 1;
+		 return 2;
 	  }
 
 	  public String getColumnName(int i) {
 	    // Challenge!
-		  return "Column name 1";
+		  return columnNames[i];
 	  }
 
 	  public int getRowCount() {
 	    // Challenge!
-		  return 1;
+		  int suma = 0;
+          for(Covid19Pacient p : pacients)
+              suma += p.getSymptoms().size();
+          return suma;
 	  }
 
 	  public Object getValueAt(int row, int col) {
-	    // Challenge!
-		  return "value";
-	  }
+	
+		  int currentRow = 0;
+          for (Covid19Pacient p : pacients) {
+              for (Symptom s : p.getSymptoms()) {
+                  if (currentRow == row) {
+                      switch (col) {
+                          case 0:
+                              return s.getName();
+                          case 1:
+                              return p.getWeight(s);
+                      }
+                  }
+                  currentRow++;
+              }
+          }
+          return null;
+	 
+	    }
+	  
 	}
